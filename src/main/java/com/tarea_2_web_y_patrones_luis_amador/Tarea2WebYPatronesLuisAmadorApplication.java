8e7cd9b53541e5714d881212d0a3f0479cf4c5df
Luis <Luis@LUIS-AMADOR-PC-MSI>
2022-06-04 21:07:53 -0600

    Commit 1 de Tarea 2 de Web y Patrones

---
+++ src/main/java/com/tarea_2_web_y_patrones_luis_amador/Tarea2WebYPatronesLuisAmadorApplication.java
@@ -0,0 +1,13 @@
+package com.tarea_2_web_y_patrones_luis_amador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2WebYPatronesLuisAmadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2WebYPatronesLuisAmadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea_2_web_y_patrones_luis_amador/Tarea2WebYPatronesLuisAmadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea_2_web_y_patrones_luis_amador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2WebYPatronesLuisAmadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
